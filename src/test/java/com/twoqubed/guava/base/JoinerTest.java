914bb80acbcf683edc6feace7110610ad2ab228e
Ryan Breidenbach <ryan.breidenbach+github@gmail.com>
2011-06-24 07:39:50 -0500

    More intro in presentation.

--- src/test/java/com/twoqubed/guava/base/JoinerTest.java
+++ src/test/java/com/twoqubed/guava/base/JoinerTest.java
@@ -1,4 +1,18 @@
 package com.twoqubed.guava.base;
 
+import com.google.common.base.Joiner;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class JoinerTest {
+
+    @Test
+    public void shouldHandleNulls() {
+        String result = Joiner.on("|")
+                .skipNulls()
+                .join("foo", null, "bar");
+
+        assertEquals("foo|bar", result);
+    }
 }
--- src/test/java/com/twoqubed/guava/base/SplitterTest.java
+++ src/test/java/com/twoqubed/guava/base/SplitterTest.java
@@ -1,4 +1,30 @@
 package com.twoqubed.guava.base;
 
+import com.google.common.base.Splitter;
+import org.junit.Test;
+
+import static com.google.common.collect.Lists.*;
+import static org.junit.Assert.*;
+
 public class SplitterTest {
+
+    @Test
+    public void shouldOmitEmptyStrings() {
+       Splitter splitter = Splitter.on(',')
+               .omitEmptyStrings();
+
+        Iterable<String> result = splitter.split("foo,bar,,baz");
+
+        assertEquals(newArrayList("foo", "bar", "baz"), newArrayList(result));
+    }
+
+    @Test
+    public void shouldTrimWhiteSpaceStrings() {
+       Splitter splitter = Splitter.on(',')
+               .trimResults();
+
+        Iterable<String> result = splitter.split("foo, bar, baz");
+
+        assertEquals(newArrayList("foo", "bar", "baz"), newArrayList(result));
+    }
 }
